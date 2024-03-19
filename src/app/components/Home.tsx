f85e35083fad7e1249f84cdf083a1973b4551a79
vkth <ghieianagee@gmail.com>
2024-03-19 14:33:36 +0800

    Adjust font size #6

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -8,10 +8,10 @@ const Home = () => {
     <section id="home" className="min-h-screen flex items-center justify-center bg-gray-800 w-full">
     <div className="flex pl-16 pr-16 py-24 items-center text-center">
       <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-      <h2 className="text-xs text-white tracking-widest font-medium title-font mb-1">Welcome to</h2>
-        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-large text-white text-shadow text-bold">Astra Interactive Studios
+      <h2 className="text-base text-white tracking-widest font-medium title-font mb-1">Welcome to</h2>
+        <h1 className="title-font sm:text-4xl text-5xl mb-4 font-large text-white text-shadow text-bold">Astra Interactive Studios
         </h1>
-        <p className="mb-8 leading-relaxed text-white ">Where imagination meets innovation! Our studio pioneers in crafting captivating web3 and casual games that redefine entertainment. Explore our world of immersive gaming experiences and unlock the extraordinary.</p>
+        <p className="mb-8 leading-relaxed text-white text-lg">Where imagination meets innovation! Our studio pioneers in crafting captivating web3 and casual games that redefine entertainment. Explore our world of immersive gaming experiences and unlock the extraordinary.</p>
         <div className="flex justify-center">
           <button className="ml-4 inline-flex text-white py-2 px-6 border-2 border-gray-300 focus:outline-none hover:bg-gray-300 rounded text-lg">Lorem Ipsum</button>
         </div>
