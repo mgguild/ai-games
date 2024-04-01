37bc7eeb46d0a8109c0942f5b1e94dcde6526f7e
John Anthony Balbin <61826995+swengr-janan@users.noreply.github.com>
2024-04-01 18:51:54 +0800

    Change Games to Studios, Update logo and Remove buttons

--- src/app/contact/ContactUs.tsx
+++ src/app/contact/ContactUs.tsx
@@ -1,23 +1,50 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
 const ContactUs = () => {
   return (
     <>
-      <section id="contact_us" className="text-gray-700 body-font relative bg-gray-800">
+      <section
+        id="contact_us"
+        className="text-gray-700 body-font relative bg-gray-800"
+      >
         <div className=" flex px-16 py-24 md:flex-row flex-col items-center">
           <div className="container">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">CONTACT US</h1>
-            <p className="leading-relaxed mb-5 text-white">Got questions, ideas, or collaboration inquiries? Reach out to us! We’re here to listen and connect. </p>
-            <p className="leading-relaxed mb-5 text-white">Drop us a message at <a href="mailto:astrainteractivestudio@gmail.com">astrainteractivestudio@gmail.com</a>, and our team will get back to you promptly. For updates and insights, follow us on [social media handles].</p>
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+              CONTACT US
+            </h1>
+            <p className="leading-relaxed mb-5 text-white">
+              Got questions, ideas, or collaboration inquiries? Reach out to us!
+              We’re here to listen and connect.{" "}
+            </p>
+            <p className="leading-relaxed mb-5 text-white">
+              Drop us a message at{" "}
+              <a href="mailto:astrainteractivestudio@gmail.com">
+                astrainteractivestudio@gmail.com
+              </a>
+              , and our team will get back to you promptly. For updates and
+              insights, follow us on our social media pages.
+            </p>
             <Image src="/Contact_Us.png" alt="logo" width={400} height={300} />
           </div>
           <div className="container">
             <div className="lg:w-full md:w-full rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative">
-              <input className="rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4" placeholder="Email" type="email"/>
-              <input className="rounded border border-gray-400 focus:outline-none text-base px-4 py-2 mb-4" placeholder="Subject"/>
-              <textarea className="bg-white rounded border border-gray-400 focus:outline-none h-32 focus:border-indigo-500 text-base px-4 py-2 mb-4 resize-none" placeholder="Message"></textarea>
-              <button className="text-white bg-gray-300 border-0 py-2 px-16 focus:outline-none hover:bg-indigo-600 rounded text-lg">Send Email</button>
+              <input
+                className="rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4"
+                placeholder="Email"
+                type="email"
+              />
+              <input
+                className="rounded border border-gray-400 focus:outline-none text-base px-4 py-2 mb-4"
+                placeholder="Subject"
+              />
+              <textarea
+                className="bg-white rounded border border-gray-400 focus:outline-none h-32 focus:border-indigo-500 text-base px-4 py-2 mb-4 resize-none"
+                placeholder="Message"
+              ></textarea>
+              <button className="text-white bg-gray-300 border-0 py-2 px-16 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                Send Email
+              </button>
             </div>
           </div>
         </div>
