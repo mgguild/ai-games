6bcc69b348704c5bc2517ef4eb61fd46f9a6e549
John Anthony Balbin <61826995+swengr-janan@users.noreply.github.com>
2024-04-03 12:49:00 +0800

    Change Studio to Studios

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,28 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export const metadata: Metadata = {
-  title: 'Astra Interactive Studio',
-  description: 'Astra Interactive Studio',
-}
+  title: "Astra Interactive Studios",
+  description: "Astra Interactive Studios",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
         <div>
-        <Navbar />
-        <main className='relative overflow-hidden'>
-        {children}
-        </main> 
-        <Footer />
+          <Navbar />
+          <main className="relative overflow-hidden">{children}</main>
+          <Footer />
         </div>
       </body>
     </html>
-  )
+  );
 }
