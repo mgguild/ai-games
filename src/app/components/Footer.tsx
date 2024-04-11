cc0b1cbaba36a2d4a0c39492fb7c4a2049aae514
vkth <ghieianagee@gmail.com>
2024-04-11 17:56:58 +0800

    Fix about us text alignments

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,22 +5,21 @@ const Footer = () => {
   return (
     <>
       <footer className="text-gray-700 body-font">
-        <div className="container pt-14 mx-auto">
-          <div className="flex flex-wrap md:text-left text-center -mb-10 ">
+        <div className="container pt-14 pb-10 mx-auto">
+          <div className="flex flex-wrap items-center justify-center text-center "> 
             <div className="lg:w-2/6 md:w-1/2 w-full px-4">
-                  <a href="https://astrainteractive.studios" className="inline-flex lg:ml-auto lg:mt-0 mt-6 w-full justify-center md:justify-start md:w-auto mb-5">
-                    <Image
-                      src="/aig_logo_small_black.png"
-                      alt="logo"
-                      width={50}
-                      height={50}
-                    />
-                  </a>
-                    <p className="lg:w-full text-xs text-gray-800 inline-flex lg:ml-auto lg:mt-0 mt-6 w-full justify-center md:justify-start md:w-auto">
-                      {" "}
-                      Copyright © 2024 Astra Interactive Studios. All rights
-                      reserved.{" "}
-                    </p>
+              <a href="https://astrainteractive.studios" className="inline-flex w-full justify-center md:justify-start md:w-auto mb-5"> 
+                <Image
+                  src="/aig_logo_small_black.png"
+                  alt="logo"
+                  width={100}
+                  height={100}
+                />
+              </a>
+              <p className="lg:w-full text-based text-gray-800 inline-flex w-full justify-center md:justify-start md:w-auto"> 
+                Copyright © 2024 Astra Interactive Studios. All rights
+                reserved.
+              </p>
             </div>
           </div>
         </div>
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -15,50 +15,47 @@ const Navbar = () => {
 
   return (
     <nav className="bg-black fixed w-full z-10 top-0 ">
-      <div className="max-w-xl mx-auto px-4 sm:px-4 lg:px-24" style={{height: "7.7vh"}}>
-        <div className="hidden md:block">
-          <div className="ml-4 flex justify-center items-center space-x-4">
-            <a href="#services" className="text-white transition duration-300 focus:outline-none focus:text-yellow-500 focus:underline hover:underline hover:text-white" onClick={closeNavbar}>
-              Services
-            </a>
-            <a href="#games" className="text-white transition duration-300 focus:outline-none focus:text-yellow-500 focus:underline hover:underline hover:text-white" onClick={closeNavbar}>
-              Games
-            </a>
-            <a href="" className="" onClick={closeNavbar}> 
-              <Image src="/aig_logo_small.png" alt="logo" width={50} height={40} objectPosition='relative' />
-            </a>
-            <a href="#about_us" className="text-white transition duration-300 focus:outline-none focus:text-yellow-500 focus:underline hover:underline hover:text-white" onClick={closeNavbar}>
-              About 
-            </a>
-            <a href="#contact_us" className="text-white transition duration-300 focus:outline-none focus:text-yellow-500 focus:underline hover:underline hover:text-white" onClick={closeNavbar}>
-              Contact
-            </a>
-          </div>
-        </div>
-        <div className="md:hidden flaot-right">
-          <div className="flex items-center">
-            <div className="flex-shrink-0">
-              <a href="/" className="text-white" onClick={closeNavbar}>
-                <Image src="/aig_logo_small.png" alt="logo" width={50} height={40} objectPosition='relative' />
-              </a>
-            </div>
-            <button className="inline-flex p-4 rounded-md text-white md:text-white hover:text-white focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white ml-auto" onClick={toggleNavbar}>
-              {isClick ? (
-                <svg className="h-8 w-9" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12"/>
-                </svg>
-              ) : (
-                <svg className="h-8 w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7"/>
-                </svg>
-              )}
-            </button>
-          </div>
+    <div className="max-w-xl mx-auto px-4 sm:px-4 lg:px-24" style={{ height: "7.7vh" }}>
+    <div className="flex items-center"> 
+      <div className="flex-shrink-0 mr-4"> 
+        <a href="/" className="text-white" onClick={closeNavbar}>
+          <Image src="/aig_logo_small.png" alt="logo" width={50} height={40} objectPosition='relative' />
+        </a>
+      </div>
+      <div className="hidden md:block">
+        <div className="ml-4 flex justify-center items-center space-x-4">
+          <a href="#services" className="text-white transition duration-300 focus:outline-none focus:text-yellow-500 focus:underline hover:underline hover:text-white" onClick={closeNavbar}>
+            Services
+          </a>
+          <a href="#games" className="text-white transition duration-300 focus:outline-none focus:text-yellow-500 focus:underline hover:underline hover:text-white" onClick={closeNavbar}>
+            Games
+          </a>
+          <a href="#about_us" className="text-white transition duration-300 focus:outline-none focus:text-yellow-500 focus:underline hover:underline hover:text-white" onClick={closeNavbar}>
+            About
+          </a>
+          <a href="#contact_us" className="text-white transition duration-300 focus:outline-none focus:text-yellow-500 focus:underline hover:underline hover:text-white" onClick={closeNavbar}>
+            Contact
+          </a>
         </div>
       </div>
+      <div className="md:hidden ml-auto"> 
+        <button className="inline-flex p-4 rounded-md text-white md:text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" onClick={toggleNavbar}>
+          {isClick ? (
+            <svg className="h-8 w-9" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          ) : (
+            <svg className="h-8 w-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
+            </svg>
+          )}
+        </button>
+      </div>
+    </div>
+  </div>
       {isClick && (
-        <div className="md:hidden float-right">
-          <div className="pl-5 pr-12 pt-5 pb-9 space-y-2 gap-4 sm:px-3">
+        <div className="md:hidden text-center">
+          <div className="pl-5 pr-5 pt-5 pb-9 space-y-2 gap-4 sm:px-3">
             <a href="#" className="text-white text-2xl leading-10 block transition duration-300 focus:outline-none focus:text-yellow-500 focus:underline hover:underline hover:text-white" onClick={closeNavbar}>
               Home
             </a> 
