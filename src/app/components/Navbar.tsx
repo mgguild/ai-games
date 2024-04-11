f689cf553c25286b959c83079cf99ce803ee972b
vkth <ghieianagee@gmail.com>
2024-04-11 13:58:44 +0800

    Remove Contact Us section #20

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -15,9 +15,9 @@ const Navbar = () => {
 
   return (
     <nav className="bg-black fixed w-full z-10 top-0 ">
-      <div className="max-w-xl mx-auto px-6 sm:px-6 lg:px-24">
+      <div className="max-w-xl mx-auto px-4 sm:px-4 lg:px-24" style={{height: "7.7vh"}}>
         <div className="hidden md:block">
-          <div className="ml-4 flex items-center space-x-4">
+          <div className="ml-4 flex justify-center items-center space-x-4">
             <a href="#services" className="text-white transition duration-300 focus:outline-none focus:text-yellow-500 focus:underline hover:underline hover:text-white" onClick={closeNavbar}>
               Services
             </a>
@@ -42,7 +42,7 @@ const Navbar = () => {
                 <Image src="/aig_logo_small.png" alt="logo" width={50} height={40} objectPosition='relative' />
               </a>
             </div>
-            <button className="inline-flex p-6 rounded-md text-white md:text-white hover:text-white focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white ml-auto" onClick={toggleNavbar}>
+            <button className="inline-flex p-4 rounded-md text-white md:text-white hover:text-white focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white ml-auto" onClick={toggleNavbar}>
               {isClick ? (
                 <svg className="h-8 w-9" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12"/>
@@ -58,17 +58,20 @@ const Navbar = () => {
       </div>
       {isClick && (
         <div className="md:hidden float-right">
-          <div className="px-4 pt-4 pb-6 space-y-2 gap-4 sm:px-3">
-            <a href="#services" className="text-white block transition duration-300 focus:outline-none focus:text-yellow-500 focus:underline hover:underline hover:text-white" onClick={closeNavbar}>
+          <div className="pl-5 pr-12 pt-5 pb-9 space-y-2 gap-4 sm:px-3">
+            <a href="#" className="text-white text-2xl leading-10 block transition duration-300 focus:outline-none focus:text-yellow-500 focus:underline hover:underline hover:text-white" onClick={closeNavbar}>
+              Home
+            </a> 
+            <a href="#services" className="text-white text-2xl leading-10 block transition duration-300 focus:outline-none focus:text-yellow-500 focus:underline hover:underline hover:text-white" onClick={closeNavbar}>
               Services
             </a>
-            <a href="#games" className="text-white block transition duration-300 focus:outline-none focus:text-yellow-500 focus:underline hover:underline hover:text-white" onClick={closeNavbar}>
+            <a href="#games" className="text-white text-2xl leading-10 block transition duration-300 focus:outline-none focus:text-yellow-500 focus:underline hover:underline hover:text-white" onClick={closeNavbar}>
               Games
             </a>
-            <a href="#about_us" className="text-white block transition duration-300 focus:outline-none focus:text-yellow-500 focus:underline hover:underline hover:text-white" onClick={closeNavbar}>
+            <a href="#about_us" className="text-white text-2xl leading-10 block transition duration-300 focus:outline-none focus:text-yellow-500 focus:underline hover:underline hover:text-white" onClick={closeNavbar}>
               About 
             </a>
-            <a href="#contact_us" className="text-white block transition duration-300 focus:outline-none focus:text-yellow-500 focus:underline hover:underline hover:text-white" onClick={closeNavbar}>
+            <a href="#contact_us" className="text-white text-2xl leading-10 block transition duration-300 focus:outline-none focus:text-yellow-500 focus:underline hover:underline hover:text-white" onClick={closeNavbar}>
               Contact
             </a>
           </div>
--- src/app/services/Services.tsx
+++ src/app/services/Services.tsx
@@ -5,56 +5,80 @@ const Services = () => {
   return (
         <section id="services" className="text-gray-700 body-font min-h-screen ">
         <div className=" pl-16 pr-16 py-24 items-center md:px-0">
-            <div className="flex flex-col text-center w-full mb-15 pt-4">
-                <h1 className="title-font sm:text-4xl text-4xl mb-4 font-medium text-gray-900">Our Services</h1>
+            <div className="flex flex-col text-center w-full mb-15 pt-4 lg:pb-0 md:pb-32">
+                <h1 className="title-font sm:text-4xl text-5xl mb-4 font-medium text-gray-900">Our Services</h1>
                 <div className="border-t border-black w-1/4 mx-auto mb-4"></div>
-                <h2 className="text-base text-black tracking-widest font-medium title-font mb-1">
+                <h2 className="text-base text-black tracking-widest font-medium title-font mb-1 pb-8">
                     At Astra Interactive Studios, we offer a spectrum of services tailored to elevate your gaming journey:</h2>
             </div>
             
-            <div className="container mx-auto flex px-16 py-24 md:flex-row flex-col items-center">
-                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-                <Image src="/Game_Development.png" alt="logo" width={400} height={300} objectPosition='relative'/>
+            <div className="container mx-auto flex lg:px-36 lg:py-24 md:flex-row flex-col md:py-1 md:px-1 items-center">
+                
+                {/* desktop view only */}
+                <div className="hidden lg:block lg:max-w-lg lg:w-full md:w-full w-full">
+                    <Image src="/Game_Development.png" alt="logo" width={400} height={300} objectPosition='relative'/>
                 </div>
+
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                    <h1 className="title-font sm:text-4xl text-4xl mb-4 font-medium text-gray-900">Game Development
+                    <h1 className="title-font sm:text-4xl text-4xl mb-2 font-medium text-gray-900">Game Development
                     </h1>
-                    <p className="mb-8 leading-relaxed text-lg">From conceptualization to launch, we breathe life into your ideas, leveraging cutting-edge technologies and creative prowess.</p>
+                    <p className="mb-2 leading-relaxed text-lg">From conceptualization to launch, we breathe life into your ideas, leveraging cutting-edge technologies and creative prowess.</p>
                     <div className="flex justify-center">
                     {/* <button className="ml-4 inline-flex text-black py-2 px-6 border-2 border-gray-300 focus:outline-none hover:bg-gray-300 rounded text-lg">Lorem Ipsum</button> */}
                     </div>
                 </div>
+
+                {/* mobile view only */}
+                <div className="block lg:hidden lg:max-w-lg lg:w-full md:w-full w-full pb-32">
+                    <Image src="/Game_Development.png" alt="logo" width={400} height={300} objectPosition='relative'/>
+                </div>
+
             </div>
 
-            <div className="container mx-auto flex px-16 py-24 md:flex-row flex-col items-center">
+            <div className="container mx-auto flex lg:px-36 lg:py-24 md:flex-row flex-col md:py-1 md:px-1 items-center">
+                    <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Web3 Integration
+                        </h1>
+                        <p className="mb-8 leading-relaxed text-lg">Embrace the future of gaming with seamless integration of blockchain and web3 functionalities, revolutionizing gameplay and ownership.</p>
+                        <div className="flex justify-center">
+                        {/* <button className="ml-4 inline-flex text-black py-2 px-6 border-2 border-gray-300 focus:outline-none hover:bg-gray-300 rounded text-lg">Lorem Ipsum</button> */}
+                        </div>
+                    </div>
+
+                    {/* desktop view only */}
+                    <div className="hidden lg:block lg:max-w-lg lg:w-full md:w-full w-full">
+                    <Image src="/Web3_Integration.png" alt="logo" width={400} height={300} objectPosition='relative'/>
+                    </div>
+
+                    {/* mobile view only */}
+                    <div className="block lg:hidden lg:max-w-lg lg:w-full md:w-full w-full pb-32">
+                    <Image src="/Web3_Integration.png" alt="logo" width={400} height={300} objectPosition='relative'/>
+                    </div>
+            </div>
+
+            <div className="container mx-auto flex lg:px-36 lg:py-24 md:flex-row flex-col md:py-1 md:px-1 items-center">
+
+                 {/* desktop view only */}
+                 <div className="hidden lg:block lg:max-w-lg lg:w-full md:w-full w-full">
+                <Image src="/Consultation.png" alt="logo" width={400} height={300} objectPosition='relative'/>
+                </div>
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Web3 Integration
-                </h1>
-                <p className="mb-8 leading-relaxed text-lg">Embrace the future of gaming with seamless integration of blockchain and web3 functionalities, revolutionizing gameplay and ownership.</p>
-                <div className="flex justify-center">
-                {/* <button className="ml-4 inline-flex text-black py-2 px-6 border-2 border-gray-300 focus:outline-none hover:bg-gray-300 rounded text-lg">Lorem Ipsum</button> */}
+                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Consultation
+                    </h1>
+                    <p className="mb-8 leading-relaxed text-lg">Our team of experts offers comprehensive guidance, ensuring your project aligns with industry trends and user preferences.</p>
+                    <div className="flex justify-center">
+                    {/* <button className="ml-4 inline-flex text-black py-2 px-6 border-2 border-gray-300 focus:outline-none hover:bg-gray-300 rounded text-lg">Lorem Ipsum</button> */}
+                    </div>
                 </div>
+                 {/* mobile view only */}
+                 <div className="block lg:hidden lg:max-w-lg lg:w-full md:w-full w-full pb-32">
+                <Image src="/Consultation.png" alt="logo" width={400} height={300} objectPosition='relative'/>
                 </div>
-                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-                <Image src="/Web3_Integration.png" alt="logo" width={400} height={300} objectPosition='relative'/>
-                </div>
+
             </div>
 
-            <div className="container mx-auto flex px-16 py-24 md:flex-row flex-col items-center">
-            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-            <Image src="/Consultation.png" alt="logo" width={400} height={300} objectPosition='relative'/>
-            </div>
-            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Consultation
-                </h1>
-                <p className="mb-8 leading-relaxed text-lg">Our team of experts offers comprehensive guidance, ensuring your project aligns with industry trends and user preferences.</p>
-                <div className="flex justify-center">
-                {/* <button className="ml-4 inline-flex text-black py-2 px-6 border-2 border-gray-300 focus:outline-none hover:bg-gray-300 rounded text-lg">Lorem Ipsum</button> */}
-                </div>
-            </div>
-            </div>
+            <div className="container mx-auto flex lg:px-36 lg:py-24 md:flex-row flex-col md:py-1 md:px-1 items-center">
 
-            <div className="container mx-auto flex px-16 py-24 md:flex-row flex-col items-center">
             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                 <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Custom Solutions
                 </h1>
@@ -63,10 +87,12 @@ const Services = () => {
                 {/* <button className="ml-4 inline-flex text-black py-2 px-6 border-2 border-gray-300 focus:outline-none hover:bg-gray-300 rounded text-lg">Lorem Ipsum</button> */}
                 </div>
             </div>
-            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            
+            <div className="lg:max-w-lg lg:w-full md:w-full w-full">
                 <Image src="/Custom_Solutions.png" alt="logo"width={400} height={300} objectPosition='relative'/>
                 </div>
             </div>
+
             </div>
         </section>
   )
