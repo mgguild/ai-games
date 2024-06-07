1a9bf8281f5b058401765e9e08bc71bdf72e9b24
vkth <ghieianagee@gmail.com>
2024-06-07 16:37:17 +0800

    Add privacy policy for helltoons

---
+++ src/app/components/Helltoons.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Helltoons= () => {
+  return (
+    
+    <section
+      id="helltoons"
+      className="flex sm:pl-16 pl-1 sm:pr-16 pr-1 py-24 mt-6  flex-col md:flex-row bg-gradient-to-br from-yellow-200 to-red-300 border-8 border-white ">
+        
+        <div className="bg-white rounded-3xl leading-10">
+        <div className="container mx-auto px-4 py-8">
+            <h1 className="text-3xl font-bold mb-4">Privacy Policy</h1>
+            <p className="mb-4">Your privacy is important to us. It is Helltoons: Card Clash's policy to respect your privacy regarding any information we may collect from you across our game, and other sites we own and operate.</p>
+            <p className="mb-4">We only ask for personal information when we truly need it to provide service to you. We collect it by fair and lawful means, with your knowledge and consent. We also let you know why we’re collecting it and how it will be used.</p>
+            <p className="mb-4">We only retain collected information for as long as necessary to provide you with your requested service. What data we store, we’ll protect within commercially acceptable means to prevent loss and theft, as well as unauthorized access, disclosure, copying, use, or modification</p>
+            <p className="mb-4">We don’t share any personally identifying information publicly or with third parties, except when required to by law.</p>
+            <p className="mb-4">Our game may link to external sites that are not operated by us. Please be aware that we have no control over the content and practices of these sites, and cannot accept responsibility or liability for their respective privacy policies.</p>
+            <p className="mb-4">You are free to refuse our request for your personal information, with the understanding that we may be unable to provide you with some of your desired services.</p>
+            <p className="mb-4">Your continued use of our website will be regarded as an acceptance of our practices around privacy and personal information. If you have any questions about how we handle user data and personal information, feel free to contact us.</p>
+            <p className="mb-4">This policy is effective as of June 2024.</p>
+        </div>
+        </div>
+    </section>
+  );
+};
+
+export default Helltoons;
