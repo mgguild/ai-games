8a56bbb61e55ad6719ed9831b593627cd10a788c
vkth <ghieianagee@gmail.com>
2024-07-31 01:10:51 +0800

    Add helltoons preview of cards #68

--- src/app/helltoons/Aboutpage.tsx
+++ src/app/helltoons/Aboutpage.tsx
@@ -14,30 +14,30 @@ const Aboutpage = () => {
  
         <div className="flex flex-row justify-center items-stretch mb-5">
           <div className="flex flex-col text-left w-full lg:w-full lg:pr-4 lg:pb-0 md:pb-32">
-            <h1 className="sm:text-2xl text-2xl px-16 mb-6 font-bold text-black">About</h1>
+            <h1 className="sm:text-2xl text-2xl px-16 mb-6 font-bold text-gray-800">About</h1>
             <p className="sm:pl-16 pl-3 pb-5 leading-none sm:text-xl text-sm text-gray-800 font-medium mb-5">
-              The King of Hell has fallen! The denizens have scrambled to take the throne of the Overlord. Team up with your favorite sinners and wallop the opposition or get pummeled by psychos! RULE TOGETHER or DOUBLE-CROSS EACH OTHER!
+              The King of Hell has fallen! The denizens have scrambled to take the throne of the Overlord. Team up with your favorite sinners and wallop the opposition or get pummeled by psychos! <b>RULE TOGETHER</b> or <b>DOUBLE-CROSS EACH OTHER!</b>
             </p>
             <p className="sm:pl-16 pl-3 pb-5 leading-none sm:text-xl text-sm text-gray-800 font-medium mb-5">
-              Helltoons: Card Clash is a card-collecting game with short but tense matches! Build your best team with the cards you have and discover synergies between them. Or be the fun police and predict and counter opposing teams!
+              <b>Helltoons: Card Clash</b> is a card-collecting game with short but tense matches! Build your best team with the cards you have and discover synergies between them. Or be the fun police and predict and counter opposing teams!
               Collect 60 unique cards with synergies, and wacky effects that can make or break your team. Visit the bounty board! Winning against these foes gets one of their cards to join you.
             </p>
           </div>
           <div className="flex flex-col text-center w-full lg:w-1/2 lg:pl-4 lg:pb-0 md:pb-32">
             <div className="flex flex-col text-right">
-              <h1 className="sm:text-2xl pr-16 text-2xl mb-6 font-bold text-black">Release Date</h1>
+              <h1 className="sm:text-2xl pr-16 text-2xl mb-6 font-bold text-gray-800">Release Date</h1>
               <p className="sm:pl-16 pl-3 pr-16 pb-5 leading-none sm:text-xl text-xs text-gray-800 font-medium mb-5">
                 ETA August 2024
               </p>
             </div>
             <div className="flex flex-col text-right">
-              <h1 className="sm:text-2xl pr-16 text-2xl mb-6 font-bold text-black">Publisher</h1>
+              <h1 className="sm:text-2xl pr-16 text-2xl mb-6 font-bold text-gray-800">Publisher</h1>
               <p className="sm:pl-16 pl-3 pr-16 pb-5 leading-none sm:text-xl text-xs text-gray-800 font-medium mb-5">
                 Astra Interactive Studios
               </p>
             </div>
             <div className="flex flex-col text-right">
-              <h1 className="sm:text-2xl pr-16 text-2xl mb-6 font-bold text-black">Rating</h1>
+              <h1 className="sm:text-2xl pr-16 text-2xl mb-6 font-bold text-gray-800">Rating</h1>
               <p className="sm:pl-16 pl-3 pr-16 pb-5 leading-none sm:text-xl text-xs text-gray-800 font-medium mb-5">
                 Teen
               </p>
--- src/app/helltoons/page.tsx
+++ src/app/helltoons/page.tsx
@@ -1,5 +1,6 @@
 import Aboutpage from "./Aboutpage";
 import Helltoons from "./Homepage";
+import PreviewCards from "./PreviewCards";
 
 export default function Hero(){
     return (
@@ -9,6 +10,9 @@ export default function Hero(){
         </div>
         <div id="aboutpage">
             <Aboutpage />
+        </div>
+        <div id="aboutpage">
+            <PreviewCards />
         </div>
         </>
     )
