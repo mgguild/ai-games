662953daf8994fd857c399e95a942b2fb2c84286
vkth <ghieianagee@gmail.com>
2024-08-27 19:33:38 +0800

    Fix ipad stretch video bug #86

--- src/app/helltoons/Homepage.tsx
+++ src/app/helltoons/Homepage.tsx
@@ -12,9 +12,9 @@ const Helltoons = () => {
           autoPlay
           loop
           muted
-          className="hidden lg:block absolute top-0 left-0 w-full h-full object-fill z-[-10]"
+          className="hidden lg:block absolute top-0 left-0 w-full h-full object-cover z-[-10]"
         >
-          <source src="/helltoons/Helltoons Landing Page_2.mp4" type="video/mp4" />
+          <source src="/helltoons/helltoons1.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
 
