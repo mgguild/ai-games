d37e6b35cafbc1be4aed6c538b20c7cbcaae9dd7
vkth <42258463+vkth@users.noreply.github.com>
2024-12-26 17:16:28 +0800

    Udpdate link for metasagawarriors game portfolio

--- src/app/games/Games.tsx
+++ src/app/games/Games.tsx
@@ -56,7 +56,7 @@ const Games = () => {
                     </div>
                     <div className="relative p-4 m-6 flex flex-col items-center justify-center" style={{ height: '460px', width: '100%', maxWidth: '340px' }}>
                       <div className="relative z-15 flex justify-center mb-4">
-                        <a href="https://msw.gg" target="_blank" rel="noopener noreferrer" className="transition-transform transform hover:scale-105">  
+                        <a href="https://metasagawarriors.com/" target="_blank" rel="noopener noreferrer" className="transition-transform transform hover:scale-105">  
                           <Image src="/METASAGA WARRIORS.png" alt="icon" width={460} height={240} />
                         </a>
                       </div>
