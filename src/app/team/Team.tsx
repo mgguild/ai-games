4599f4dc0b6fa437b3e10d991d4dcff5dc409783
vkth <42258463+vkth@users.noreply.github.com>
2025-02-21 23:48:43 +0800

    Fix team position by 2s in mobile view

--- src/app/team/Team.tsx
+++ src/app/team/Team.tsx
@@ -48,7 +48,7 @@ const Team = () => {
             </div>
         </div>
         <div className="block lg:hidden">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
           {teamMembers.map((member, index) => (
             <MemberCard key={index} member={member} />
           ))}
